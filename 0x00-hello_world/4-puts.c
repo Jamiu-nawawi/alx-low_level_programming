a0ac1320ce71c7909cefe0cd80f352764a19b781
root <abubakhr.jamiu@gmail.comgit config --global user.name Jamiu-nawawigitconfig--globaluser.emailabubakhr.jamiu@gmail.comgitconfig--globaluser.emailabubakhr.jamiu@gmail.comgit config user.mail abubakhr.jamiu@gmail.com>
2022-12-08 16:30:02 -0800

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is never a child play, you always keep doing hard things);
+	return (0);
+}
