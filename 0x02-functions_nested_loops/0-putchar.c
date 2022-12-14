0ff33121d6669efee1423ef43589fe77539b21cc
root <abubakhr.jamiu@gmail.comgit config --global user.name Jamiu-nawawigitconfig--globaluser.emailabubakhr.jamiu@gmail.comgitconfig--globaluser.emailabubakhr.jamiu@gmail.comgit config user.mail abubakhr.jamiu@gmail.com>
2022-12-14 12:37:08 -0800

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * main - Print Correct as a message
- *
- * Return: Always 0 (Good)
+ * main - Print _putchar
+ *is written by nawawi
+ * Return: Always 0
  */
+
 int main(void)
 {
-	int str = {95, 112, 117, 115, 99, 104, 95, 113};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
